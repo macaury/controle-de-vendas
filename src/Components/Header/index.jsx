47a260eddb7ce95e2f03fcda2b4d8396dfb21c7b
macaury carvalho <carvalhomacaury@gmail.com>
2022-10-20 13:34:41 -0300

    master

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "./styles";
+
+const Header = () => {
+    return (
+        <C.container>
+            <C.Header>
+                <C.title>Controle de Financeiro</C.title>
+            </C.Header>
+        </C.container>
+    );
+};
+
+export default Header;
