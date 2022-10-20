1a2c36accd9afda32a93372b9ee71c80261099ed
macaury carvalho <carvalhomacaury@gmail.com>
2022-10-20 12:58:19 -0300

    Master

--- src/Components/Grid/index.jsx
+++ src/Components/Grid/index.jsx
@@ -7,5 +7,27 @@ const Grid = ({itens, setItens}) => {
         const newArray = itens.filter((transaction) => transaction.id !== ID);
         setItens(newArray);
         localStorage.setItem("transaction", JSON.stringify(newArray));
-    }
-}
+    };
+    return(
+        <>
+            <C.Table>
+                <c.Thread>
+                    <c.Tr>
+                        <c.Th width={40}>Descricao</c.Th>
+                        <c.Th width={40}>Valor</c.Th>
+                        <c.Th width={10} alighCenter>Tipo</c.Th>
+                        <c.Th width={10}></c.Th>
+                    </c.Tr>
+                </c.Thread>
+                <C.Tbody>
+                    {itens?.map((item, index) => (
+                        <GridItem key={index} item={item} onDelete={onDelete}/>
+                    ))}
+                </C.Tbody>
+            </C.Table>
+        
+        </>
+    );
+};
+
+export default Grid;
