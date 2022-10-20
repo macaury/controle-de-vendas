7c7c5c9f029e8b8b6c87ea603b910e2d4d020d7b
macaury carvalho <carvalhomacaury@gmail.com>
2022-10-20 03:08:37 -0300

    master

--- src/Components/forms/index.jsx
+++ src/Components/forms/index.jsx
@@ -25,6 +25,8 @@ const Form = ({ handleAdd,transactionsList, setTransactionsList }) => {
         };
         handleAdd(transaction);
 
+        setDesc("");
+        setAmount("");
     };
     return (
         <>
